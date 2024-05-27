8f4eb12bfb9bd0dc2ef694404f4c6adc355d16e8
Rashid Ziya <29undertheeye@gmail.com>
2024-05-27 20:04:44 +0530

    finished week 14-02 live
    finished up untill database section

--- projects/week-14/week-14-02-live/app/api/user/route.ts
+++ projects/week-14/week-14-02-live/app/api/user/route.ts
@@ -1,17 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export async function GET(){
     // DB call to fetch the data of username and the email who is signed in
-    return Response.json({name:"RashidZiya",email:"ziya@gmail.com"});
+    const user=await prisma.user.findMany();
+    return Response.json({
+        user
+    })
 }
 
 
 export async function POST(req:NextRequest,res:NextResponse){
     const body=await req.json();
-    return Response.json({
-        name:body.name,
-        email:body.email,
-        password:body.password
-    });
+    try{
+        const user= await prisma.user.create({
+            data:{
+                name:body.name,
+                email:body.email,
+                password:body.password
+            }
+        })
+        console.log(user.id)
+        return Response.json({
+            message:'You are logged in',
+            User:user
+        });
+    }catch(err){
+        console.log(err);
+    }
 }
--- projects/week-14/week-14-02-live/app/page.tsx
+++ projects/week-14/week-14-02-live/app/page.tsx
@@ -8,7 +8,7 @@ const fethData=async ()=>{
   // artificial delay here to 
   await new Promise((r)=>setTimeout(r,5000))
   const response=await axios.get('http://localhost:3000/api/user');
-  const data =response.data;
+  const data =response.data.user;
   return data;
 }
 
@@ -17,12 +17,15 @@ export default async function Home() {
   return (
     <div className="flex flex-col justify-center h-screen">
     <div className="flex justify-center">
-        <div className="border p-8 rounded">
-            <div>
-                Name: {userData?.name}
-            </div>
-            Email: {userData?.email}
-        </div>
+      {userData.map((user)=>{
+          return <div className="border p-8 rounded mt-2 ">
+              <div>
+                  Name: {user?.name}
+              </div>
+              Email: {user?.email}
+          </div>
+      })}
+        
     </div>
 </div>
   );
