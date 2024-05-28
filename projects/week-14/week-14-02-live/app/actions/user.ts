91f808a1cb34b34910c13c3f3e2a252cc73e31c0
Rashid Ziya <29undertheeye@gmail.com>
2024-05-28 18:58:10 +0530

    Need to implement and understand more Server Actions concept

---
+++ projects/week-14/week-14-02-live/app/actions/user.ts
@@ -0,0 +1,10 @@
+"use server"
+
+// here to implement the logic/function to implement server actions
+// Server Actions :- It contains the function that simply can be accessed by both 
+// server and client component. like if we want a generic fucntion which fetches some 
+// kind of data from the DB. and it is needed in both server  and client. component then
+// in those cases we can make a generic function using "server actions" which can then 
+// be easily accesible from client and server component as function() call.
+
+// get Full understanding and then implement here.
