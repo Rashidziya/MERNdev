c3dbb269bd68995035a86bddff508473bec251a0
Rashid Ziya <29undertheeye@gmail.com>
2024-05-28 18:37:16 +0530

    week 14-02 offline
    Added better fetches

--- projects/week-14/week-14-02-live/app/page.tsx
+++ projects/week-14/week-14-02-live/app/page.tsx
@@ -1,32 +1,28 @@
 import axios from "axios";
-import Image from "next/image";
-import { resolve } from "path";
-import { useState } from "react";
-
+import { PrismaClient } from "@prisma/client";
+const prisma=new PrismaClient();
 
 const fethData=async ()=>{
-  // artificial delay here to 
-  await new Promise((r)=>setTimeout(r,5000))
-  const response=await axios.get('http://localhost:3000/api/user');
-  const data =response.data.user;
-  return data;
+  // // artificial delay here to 
+  // await new Promise((r)=>setTimeout(r,5000))
+  // const response=await axios.get('http://localhost:3000/api/user');
+  // const data =response.data.user;
+  // return data;
+
+  const res=await prisma.user.findMany({});
+  return res;
 }
 
 export default async function Home() {
   const userData=await fethData();
   return (
-    <div className="flex flex-col justify-center h-screen">
-    <div className="flex justify-center">
-      {userData.map((user)=>{
-          return <div className="border p-8 rounded mt-2 ">
-              <div>
-                  Name: {user?.name}
-              </div>
-              Email: {user?.email}
-          </div>
-      })}
-        
+    <div className="flex flex-col justify-center items-center h-screen">
+      {userData.map((user) => (
+        <div className="border p-8 rounded mt-2 w-1/3">
+          <div>Name: {user?.name}</div>
+          <div>Email: {user?.email}</div>
+        </div>
+      ))}
     </div>
-</div>
   );
 }
